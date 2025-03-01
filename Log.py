af8744c0071867e7e500d6a88c5b47eb76bc826c
YUDONGLING <YDL@YUDONGLING.NET>
2025-03-01 16:49:02 +0800

    feat(log): add write to log file with magic variables support

--- Log.py
+++ Log.py
@@ -1,3 +1,46 @@
+def MakeLog(Content: str, Path: str = 'Log/{{YYYY}}-{{MM}}-{{DD}}.txt') -> None:
+    '''
+    Make Log File with Content, Support Magic Variables for Path. \n
+
+    Table of Magic Variables: \n
+    Year         `{{YYYY}}`|`{{YY}}` ; Month            `{{MM}}`  ; Day         `{{DD}}` ;
+    Hour         `{{HH}}`            ; Minute           `{{MI}}`  ; Second      `{{SS}}` ;
+    Time Zone    `{{TZ}}`            ; Time Zone String `{{TZS}}` .
+    '''
+    import os
+    import time
+    import portalocker
+
+    Response = {
+        'Ec': 0, 'Em': '', 'Path': ''
+    }
+
+    Path = Path.replace('{{YYYY}}', time.strftime('%Y', time.localtime())) # 年　, Eg: 2021
+    Path = Path.replace('{{YY}}'  , time.strftime('%y', time.localtime())) # 年　, Eg: 21
+    Path = Path.replace('{{MM}}'  , time.strftime('%m', time.localtime())) # 月　, Eg: 01
+    Path = Path.replace('{{DD}}'  , time.strftime('%d', time.localtime())) # 日　, Eg: 01
+    Path = Path.replace('{{HH}}'  , time.strftime('%H', time.localtime())) # 时　, Eg: 00
+    Path = Path.replace('{{MI}}'  , time.strftime('%M', time.localtime())) # 分　, Eg: 00
+    Path = Path.replace('{{SS}}'  , time.strftime('%S', time.localtime())) # 秒　, Eg: 00
+    Path = Path.replace('{{TZ}}'  , time.strftime('%z', time.localtime())) # 时区, Eg: +0800
+    Path = Path.replace('{{TZS}}' , time.strftime('%Z', time.localtime())) # 时区, Eg: CST
+
+    try:
+        if os.path.dirname(Path):
+            os.makedirs(os.path.dirname(Path), exist_ok = True)
+    except Exception as Error:
+        Response['Ec'] = 50001; Response['Em'] = MakeErrorMessage(Error); return Response
+
+    try:
+        with portalocker.Lock(Path, 'a') as File:
+            File.write('[%s] %s\n' % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), Content))
+        Response['Path'] = Path
+    except Exception as Error:
+        Response['Ec'] = 50002; Response['Em'] = MakeErrorMessage(Error); return Response
+
+    return Response
+
+
 def MakeErrorMessage(Error: Exception) -> str:
     '''
     Make Error Message, Include File, Line Number and Function Name, for Debugging.
