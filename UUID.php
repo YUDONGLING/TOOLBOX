bf901217dc0606b21910c51396fef092c97474da
YUDONGLING <YDL@YUDONGLING.NET>
2025-03-01 16:53:22 +0800

    feat(uuid): support generate/decode/sort time-based UUID-like string

--- UUID.php
+++ UUID.php
@@ -22,4 +22,111 @@ function HashUUID(string $Path_Or_Data, string $Separator = "-"): string {
     ]);
 }
 
+/**
+ * Convert a Timestamp to a UUID-like String.
+ */
+function TimeUUID(int $Time = null, array $Seed = null, string $Separator = "-"): string {
+    $Time = str_pad($Time ?? time(), 10, "0", STR_PAD_LEFT);
+    $Seed = $Seed ?? [rand(0, 15), rand(0, 15)];
+    $Seed = [$Seed[0] % 16, $Seed[1] % 16];
+
+    $Hexa = "";
+    foreach (str_split($Time) as $Char) {
+        $Hexa .= sprintf("%03x", ord($Char) * ($Seed[0] % 8 + 1) * ($Seed[1] % 8 + 1));
+    }
+    $Hexa .= sprintf("%1x", $Seed[0]) . sprintf("%1x", $Seed[1]);
+
+    return implode($Separator, [
+        substr($Hexa,  0, 8),
+        substr($Hexa,  8, 4),
+        substr($Hexa, 12, 4),
+        substr($Hexa, 16, 4),
+        substr($Hexa, 20)
+    ]);
+}
+
+/**
+ * Sort Files in a Directory by TimeUUID.
+ */
+function TimeUUID_Sort(string $Path, string $Separator = null): array {
+    $Response = [
+        "Ec"    => 0,
+        "Em"    => "",
+        "Files" => []
+    ];
+
+    try {
+        $Files = scandir($Path);
+    } catch (Exception $Error) {
+        $Response["Ec"] = 50001; $Response["Em"] = MakeErrorMessage($Error); return $Response;
+    }
+
+    try {
+        $Metas = [];
+        foreach ($Files as $File) {
+            $Decoded = TimeUUID_Decode($File, $Separator);
+            if ($Decoded["Ec"] == 0) {
+                $Metas[] = [
+                    "Time" => $Decoded["Time"],
+                    "Name" => $File,
+                    "Path" => $Path . DIRECTORY_SEPARATOR . $File
+                ];
+            }
+        }
+    } catch (Exception $Error) {
+        $Response["Ec"] = 50002; $Response["Em"] = MakeErrorMessage($Error); return $Response;
+    }
+
+    try {
+        usort($Metas, function($a, $b) {
+            return $a["Time"] <=> $b["Time"];
+        });
+        $Response["Files"] = $Metas;
+    } catch (Exception $Error) {
+        $Response["Ec"] = 50003; $Response["Em"] = MakeErrorMessage($Error); return $Response;
+    }
+
+    return $Response;
+}
+
+/**
+ * Decode a UUID-like String to a Timestamp.
+ */
+function TimeUUID_Decode(string $UUID, string $Separator = null): array {
+    $Response = [
+        "Ec"   => 0,
+        "Em"   => "",
+        "Time" => -1
+    ];
+
+    try {
+        if ($Separator === null) {
+            $Separator_Len = (strlen($UUID) - 32) / 4;
+            $Separator     = $Separator_Len > 0 ? substr($UUID, 8, $Separator_Len) : "";
+        }
+    } catch (Exception $Error) {
+        $Response["Ec"] = 50001; $Response["Em"] = MakeErrorMessage($Error); return $Response;
+    }
+
+    try {
+        $UUID = str_replace($Separator, "", $UUID);
+        $Seed = [hexdec($UUID[-2]), hexdec($UUID[-1])];
+    } catch (Exception $Error) {
+        $Response["Ec"] = 50002; $Response["Em"] = MakeErrorMessage($Error); return $Response;
+    }
+
+    try {
+        $Time = "";
+        for ($i = 0; $i < strlen($UUID) - 2; $i += 3) {
+            $Hexa  = hexdec(substr($UUID, $i, 3));
+            $Time .= chr($Hexa / (($Seed[0] % 8 + 1) * ($Seed[1] % 8 + 1)));
+        }
+        $Response["Time"] = (int)$Time;
+    } catch (Exception $Error) {
+        $Response["Ec"] = 50003; $Response["Em"] = MakeErrorMessage($Error); return $Response;
+    }
+
+    return $Response;
+}
+
 ?>
--- UUID.py
+++ UUID.py
@@ -12,3 +12,89 @@ def HashUUID(Path_Or_Data: str, Separator: str = '-') -> str:
         Md5 = hashlib.md5(Path_Or_Data.encode()).hexdigest()
 
     return Separator.join([Md5[:8], Md5[8:12], Md5[12:16], Md5[16:20], Md5[20:]])
+
+
+def TimeUUID(Time: int = None, Seed: tuple[int, int] = None, Separator: str = '-') -> str:
+    '''
+    Convert a Timestamp to a UUID-like String.
+    '''
+    import time
+    import random
+
+    Time = str(int(time.time()) if Time is None else int(Time)).zfill(10)
+    Seed = (random.randint(0, 15), random.randint(0, 15)) if Seed is None else (Seed[0] % 16, Seed[1] % 16)
+    Hexa = ''.join([f'{ord(Char) * (Seed[0] % 8 + 1) * (Seed[1] % 8 + 1):03x}' for Char in Time]) + f'{Seed[0]:1x}' + f'{Seed[1]:1x}'
+
+    return Separator.join([Hexa[:8], Hexa[8:12], Hexa[12:16], Hexa[16:20], Hexa[20:]])
+
+
+def TimeUUID_Sort(Path: str, Seperator: str = None) -> dict:
+    '''
+    Sort Files in a Directory by TimeUUID.
+    '''
+    import os
+
+    if not __package__:
+          from  Log import MakeErrorMessage
+    else: from .Log import MakeErrorMessage
+
+    Response = {
+        'Ec': 0, 'Em': '', 'Files': []
+    }
+
+    try:
+        Files = os.listdir(Path)
+    except Exception as Error:
+        Response['Ec'] = 50001; Response['Em'] = MakeErrorMessage(Error); return Response
+
+    try:
+        Metas = []
+        for File in Files:
+            _ = TimeUUID_Decode(File, Seperator)
+            if _['Ec'] == 0: Metas.append({'Time': _['Time'], 'Name': File, 'Path': os.path.join(Path, File)})
+    except Exception as Error:
+        Response['Ec'] = 50002; Response['Em'] = MakeErrorMessage(Error); return Response
+    
+    try:
+        Response['Files'] = sorted(Metas, key = lambda Info: Info['Time'])
+    except Exception as Error:
+        Response['Ec'] = 50003; Response['Em'] = MakeErrorMessage(Error); return Response
+    
+    return Response
+
+
+def TimeUUID_Decode(UUID: str, Seperator: str = None) -> dict:
+    '''
+    Decode a UUID-like String to a Timestamp.
+    '''
+    if not __package__:
+          from  Log import MakeErrorMessage
+    else: from .Log import MakeErrorMessage
+
+    Response = {
+        'Ec': 0, 'Em': '', 'Time': -1
+    }
+
+    try:
+        if Seperator is None:
+            Seperator_Len = (len(UUID) - 32) // 4
+            Seperator     = UUID[8: 8 + Seperator_Len] if Seperator_Len > 0 else ''
+    except Exception as Error:
+        Response['Ec'] = 50001; Response['Em'] = MakeErrorMessage(Error); return Response
+
+    try:
+        UUID = UUID.replace(Seperator, '')
+        Seed = (int(UUID[-2], 16), int(UUID[-1], 16))
+    except Exception as Error:
+        Response['Ec'] = 50002; Response['Em'] = MakeErrorMessage(Error); return Response
+
+    try:
+        Time = ''
+        for _ in range(0, len(UUID) - 2, 3):
+            Hexa  = int(UUID[_: _ + 3], 16)
+            Time += chr(Hexa // ((Seed[0] % 8 + 1) * (Seed[1] % 8 + 1)))
+        Response['Time'] = int(Time)
+    except Exception as Error:
+        Response['Ec'] = 50003; Response['Em'] = MakeErrorMessage(Error); return Response
+
+    return Response
