f4bb8e8bea5efa21cedf099454c3c2d8a540037c
Darren Yu <YDL@YUDONGLING.NET>
2025-11-15 11:44:26 +0800

    fix(webhook): improve error handeling for parameter check

--- Webhook.py
+++ Webhook.py
@@ -1,25 +1,25 @@
-def Lark(Token: str, Topic: str = '', Message: list = []) -> dict:
+def Lark(Token: str, Topic: str = '', Message: list = None) -> dict:
     '''
     Send Message to Lark Chat Group.
     '''
     raise NotImplementedError()
 
 
-def Slack(Token: str, Topic: str = '', Message: list = []) -> dict:
+def Slack(Token: str, Topic: str = '', Message: list = None) -> dict:
     '''
     Send Message to Slack Chat Group.
     '''
     raise NotImplementedError()
 
 
-def WeChat(Token: str, Topic: str = '', Message: list = []) -> dict:
+def WeChat(Token: str, Topic: str = '', Message: list = None) -> dict:
     '''
     Send Message to Enterprise WeChat Chat Group. `Token` Should Likes `{{ACCESS_TOKEN}}|{{CHAT_ID}}`.
     '''
     raise NotImplementedError()
 
 
-def DingTalk(Token: str, Topic: str = '', Message: list = []) -> dict:
+def DingTalk(Token: str, Topic: str = '', Message: list = None) -> dict:
     '''
     Send Message to DingTalk Chat Group.
     '''
@@ -35,6 +35,8 @@ def DingTalk(Token: str, Topic: str = '', Message: list = []) -> dict:
         'Ec': 0, 'Em': ''
     }
 
+    if Message == None: Message = []
+
     try:
         MessageBody = '<font color=#6A65FF>**%s**</font> \n\n %s \n\n' % (Topic, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
@@ -42,7 +44,7 @@ def DingTalk(Token: str, Topic: str = '', Message: list = []) -> dict:
             if Item.get('Title', '') == '':
                 MessageBody += ' --- \n\n '
             else:
-                Color = Item.get('Color', 'UNKNOW').upper()
+                Color = Item.get('Color', 'UNKNOWN').upper()
                 if not Color.startswith('#') or len(Color) != 7:
                     Color = {'PURPLE': '#6A65FF', 'RED': '#FF6666', 'GREEN': '#92D050', 'BLUE': '#76CCFF'}.get(Color, '#76CCFF')
                 MessageBody += ' --- \n\n <font color=%s>**%s**</font> \n\n ' % (Color, Item.get('Title', ''))
@@ -53,7 +55,7 @@ def DingTalk(Token: str, Topic: str = '', Message: list = []) -> dict:
         Response['Ec'] = 50001; Response['Em'] = MakeErrorMessage(Error); return Response
 
     try:
-        Url = 'https://oapi.dingtalk.com/robot/send?access_token=%s' % Token.removeprefix('https://oapi.dingtalk.com/robot/send?access_token=')
+        Url = 'https://oapi.dingtalk.com/robot/send?access_token=%s' % Token.removeprefix('http://oapi.dingtalk.com/robot/send?access_token=').removeprefix('https://oapi.dingtalk.com/robot/send?access_token=')
         Hed = {
             'content-type': 'application/json'
         }
@@ -66,22 +68,22 @@ def DingTalk(Token: str, Topic: str = '', Message: list = []) -> dict:
         }
         Rsp = requests.post(Url, headers = Hed, data = json.dumps(Dat), timeout = 10).json()
 
-        if Rsp['errcode'] != 0:
-            raise Exception('%s-%s' % (Rsp['errcode'], Rsp['errmsg']))
+        if Rsp.get('errcode', -1) != 0:
+            raise Exception('%s-%s' % (Rsp.get('errcode', 'Unknown'), Rsp.get('errmsg', 'Unknown')))
     except Exception as Error:
         Response['Ec'] = 50002; Response['Em'] = MakeErrorMessage(Error); return Response
 
     return Response
 
 
-def Telegram(Token: str, Topic: str = '', Message: list = []) -> dict:
+def Telegram(Token: str, Topic: str = '', Message: list = None) -> dict:
     '''
     Send Message via Telegram Bot.
     '''
     raise NotImplementedError()
 
 
-def SeverChan(Token: str, Topic: str = '', Message: list = []) -> dict:
+def SeverChan(Token: str, Topic: str = '', Message: list = None) -> dict:
     '''
     Send Message via SeverChan.
     '''
